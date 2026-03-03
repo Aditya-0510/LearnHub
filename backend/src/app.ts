c34ad85dee8879454d7b1daa1e52a10e7a07243a
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2026-03-03 16:15:39 +0530

    CI/CD setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,18 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import userRouter from "./routers/user";
 import courseRouter from "./routers/course";
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
 
 app.use("/api/user", userRouter);
 app.use("/api/courses", courseRouter);
 
-app.listen(4000);
+export default app;
